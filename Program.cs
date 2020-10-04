1239c2b9e33c065c4ab04a5938b59bbd8f77792a
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-04 16:19:40 +0530

    [Praveen] Add: Welcome Message to Employee Wage Computation

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmployeeWageTillUC7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Message
+            Console.WriteLine("=============================================");
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Console.WriteLine("=============================================");
+        }
+    }
+}
